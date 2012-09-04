68afb62746019e02dd6b0d20be3c547435a39d55
Etienne Posthumus <eposthumus@gmail.com>
2012-09-04 13:24:52 +0200

    Add spelvar and betvar plus text of woordsoort, taal, and sfeer etc.

--- db_to_index.py
+++ db_to_index.py
@@ -1,16 +1,29 @@
 import sqlite3
 import pyes
+import nedindlexicon.templatetags.nedind_util as ntu
 
 es = pyes.ES(['localhost:9200'])
 db = sqlite3.connect('devdata.sqlite')
 cur = db.cursor()
 cur2 = db.cursor()
 cur3 = db.cursor()
-cur.execute('SELECT * from trefwoorden ORDER BY idtref')
 
+
+# Make a dict in memory for samstel caching
+cur.execute('SELECT * FROM samstel')
+samstel = {}
+for s in cur:
+    samstel.setdefault(s[3], []).append(s[0])
+cur.execute('SELECT * FROM spelvar')
+spelvar = {}
+for s in cur:
+    spelvar.setdefault(s[2], []).append(s[0])
+
+cur.execute('SELECT * FROM trefwoorden ORDER BY idtref')
 idx = batch = 0
 for trefwoord in cur:        
-    cur2.execute('SELECT betvar.meaningvar, boeken.* FROM bronnen, betvar, boeken ' +
+    cur2.execute('SELECT betvar.meaningvar, boeken.* ' +
+    'FROM bronnen, betvar, boeken ' +
     'WHERE bronnen.trefnum = ? AND bronnen.idbronnen = betvar.bronid AND boeken.idboeken = bronnen.boekid', 
     (trefwoord[5],))
 
@@ -22,26 +35,39 @@ for trefwoord in cur:
                     'boekaanduiding', 'uitgever', \
                     'plaats', 'jaartal', 'druk', 'fiction', 'lijstcode', 'genrecode', \
                     'idboeken', 'eerste_uitgave', 'annotator', 'sortorder']):
-            citaat[col] = c[i]        
+            citaat[col] = c[i]
+        # Delete these, in some they are mixed numeric and strings and cause indexing errors
+        del citaat['lijstcode']
+        del citaat['genrecode']
         body.append(citaat)
 
     cur3.execute('SELECT boeken.boekcode FROM bronnen, boeken WHERE bronnen.trefnum = ? '+
                  'AND bronnen.boekid = boeken.idboeken', (trefwoord[5],))
     boeken = [x[0] for x in cur3.fetchall()]
 
-
     data = {'trefwoord': trefwoord[0],
             'woordsoort': trefwoord[1],
+            'woordsoorttxt': ntu.exp_afk(trefwoord[1]),
             'taal': trefwoord[2],
+            'taaltxt': ntu.exp_afk(trefwoord[2]),
             'sfeer': trefwoord[3],
+            'sfeertxt': ntu.exp_afk(trefwoord[3]),
             'citaten': body,
             'trefnum': trefwoord[5],
             'betekenis': trefwoord[4],
             'boeken': boeken,
             'trefwoord_a': trefwoord[0],
              }
+    if trefwoord[5] in samstel:
+        data['samstel'] = samstel[trefwoord[5]]
+    if trefwoord[5] in spelvar:
+        data['spelvar'] = spelvar[trefwoord[5]]
     
-    es.index(data, 'nedind', 'trefwoord', trefwoord[-1])
+    try:
+        es.index(data, 'nedind', 'trefwoord', trefwoord[-1])
+    except:
+        import traceback; traceback.print_exc()
+        import pdb; pdb.set_trace()
     idx = idx + 1
     batch += 1
     if batch > 1999:
@@ -82,8 +108,6 @@ for boek in cur.fetchall():
         'jaartal': boek[7],
         'druk': boek[8],
         'fiction': boek[9],
-        'lijstcode': boek[10],
-        'genrecode': boek[11],
         'idboeken': boek[12],
         'eerste_uitgave': boek[13],
         'annotator': boek[14],
